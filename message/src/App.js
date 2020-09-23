c2b6f96e2c59b47521e94117b0477c8650b2f6e1
Divya Maheshwari <60094383+divya814@users.noreply.github.com>
2020-09-23 19:57:14 +0530

    Update App.js

--- message/src/App.js
+++ message/src/App.js
@@ -1,23 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Greet from './Components/Greet';
-import Welcome from './Components/Welcome';
 import Message from './Components/Message';
-import Counter from './Components/Counter';
 
 class App extends Component{
 render(){
   return (
     <div className="App">
-      <Counter/>
-      <Greet name="Raj" hero="Batman">
-      <p>This is a children props</p>
-      </Greet>
-      
-      <Greet name="Neha" hero="Wonder Woman"/>
-      <Welcome name="Diana" hero="Power Puff Girl"></Welcome>
-
       <Message></Message>
     </div>
   );
