9852151467a72ba934b7f7923ed5d455edc0ff72
Divya Maheshwari <60094383+divya814@users.noreply.github.com>
2020-09-23 20:00:35 +0530

    Update App.js

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Greet from './Components/Greet';
-import Welcome from './Components/Welcome';
-import Message from './Components/Message';
 import Counter from './Components/Counter';
 
 class App extends Component{
@@ -11,14 +7,6 @@ render(){
   return (
     <div className="App">
       <Counter/>
-      <Greet name="Raj" hero="Batman">
-      <p>This is a children props</p>
-      </Greet>
-      
-      <Greet name="Neha" hero="Wonder Woman"/>
-      <Welcome name="Diana" hero="Power Puff Girl"></Welcome>
-
-      <Message></Message>
     </div>
   );
 }
