e1ee5745eca670c604077f69b13cf67272194b14
Divya Maheshwari <60094383+divya814@users.noreply.github.com>
2020-09-24 22:52:28 +0530

    Add files via upload

---
+++ Event Handling/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EventBind from './Components/EventBind'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <EventBind></EventBind>
+    </div>
+  );
+}
+
+export default App;
