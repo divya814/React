740ba0f7e556b487c18d6e6598e34475f08fa695
Divya Maheshwari <60094383+divya814@users.noreply.github.com>
2020-09-24 22:57:24 +0530

    Update EventBind.js
    We can use bind() for handling events in React. There are 4 approaches through which we can handle events in React. You can try running all of them one-by-one by uncommenting them and commenting others.

--- Event Handling/src/Components/EventBind.js
+++ Event Handling/src/Components/EventBind.js
@@ -7,15 +7,18 @@ export class EventBind extends Component {
         this.state = {
              message:'hello'
         }
-        // 3rd method- binding in class constructor
+        // 3rd approach- binding in class constructor
         // this.clickHandler=this.clickHandler.bind(this)
     }
+    // 1st and 2nd approach
     // clickHandler(){
     //      this.setState({
     //          message:"goodbye!"
     //      })
 
     //  }
+    
+    // 4th approach
     clickHandler=()=>{
         this.setState({
             message:'Goodbye!'
@@ -30,12 +33,12 @@ export class EventBind extends Component {
                 {this.state.message}
 
             </div>
-            {/* 1st method- binding in render method*/}
+            {/* 1st approach- binding in render method*/}
             {/* <button onClick={this.clickHandler.bind(this)}>
                 Click
 
             </button> */}
-            {/* 2nd method- use arrow function */}
+            {/* 2nd approach- use arrow function */}
             {/* <button onClick={()=> this.clickHandler()}>Click</button> */}
             {/* 3rd approach- binding in constructor */}
             {/* 4th approach- class property as arrow functions */}
